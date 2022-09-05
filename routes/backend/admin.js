59dbcf21a1d84907ba76f3178792738f5adc3849
Sokhavuth <vuthdevelop@gmail.com>
2022-09-05 13:23:44 +0700

    Make it better

--- routes/backend/admin.js
+++ routes/backend/admin.js
@@ -24,5 +24,21 @@ router.post("/", async (req, res) => {
     admin.auth(req, res)
 })
 
+router.get("/logout", async (req, res) => {
+    const { sessionid } = req.signedCookies
+    let user = false
+    if(sessionid){
+        user = await req.mydb.session.get(sessionid)
+    }
+    
+    if(user){
+        await req.mydb.session.delete(sessionid)
+        res.clearCookie("sessionid")
+        res.redirect('/admin')
+    }else{
+        res.redirect("/admin")
+    }
+})
+
 
 module.exports = router
